d689429640157ec996ac3f19a6684055df4e3ab5
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-21 21:46:23 -0300

    añadidas rutas hijas de country

--- src/app/country/routes/country.routes.ts
+++ src/app/country/routes/country.routes.ts
@@ -1,10 +1,28 @@
 import { Routes } from '@angular/router';
 import { ByCapital } from '../pages/by-capital-page/by-capital';
+import { CountryLayout } from '../layouts/country-layout/country-layout';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapital,
+    component: CountryLayout,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapital
+      },
+
+
+
+
+
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      }
+
+
+    ]
   },
 
 ]
