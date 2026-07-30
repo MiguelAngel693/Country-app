7ff954ff51e561fe561676293f5eea8460efad43
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-30 16:23:34 -0300

    Añadidos componentes de carga

--- src/app/country/components/list/list.ts
+++ src/app/country/components/list/list.ts
@@ -11,4 +11,7 @@ import { RouterLink } from '@angular/router';
 export class List {
   countries = input.required<Country[]>();
 
+  errorMessage = input<string | undefined | Error>();
+  isLoading = input<boolean>(false);
+  isEmpty = input<boolean>(false);
 }
