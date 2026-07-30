ba2df617801b3118ec0afa43c34ee0e58c93464a
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-30 16:24:24 -0300

    Corregidos errores de asignación

--- src/app/country/mappers/country.mapper.ts
+++ src/app/country/mappers/country.mapper.ts
@@ -4,16 +4,18 @@ import { RestCountryItem } from "../interfaces/rest-countries";
 export class CountryMapper{
   static mapCountryResponseItemToCountryItem( countryItem: RestCountryItem): Country{
     const country: Country = {
-      code: countryItem.codes.alpha_3 || 'No disponible',
-      name: countryItem.names.translations["spa"].common || countryItem.names.official,
-      flag: countryItem.flag.emoji || 'No disponible',
-      flagSvg: countryItem.flag.url_svg || 'No disponible',
-      capital: countryItem.capitals[0].name || 'No disponible',
-      population: countryItem.population || 0,
-      region: this.translator(countryItem.region),
-      subregion: this.translator(countryItem.subregion),
+      code: countryItem.codes.alpha_3 ?? 'No disponible',
+      name: countryItem.names.translations["spa"].common ?? countryItem.names.official,
+      flag: countryItem.flag.emoji ?? 'No disponible',
+      flagSvg: countryItem.flag.url_svg ?? 'No disponible',
+      capital: countryItem.capitals[0]?.name ?? 'No disponible',
+      population: countryItem.population ?? 0,
+      // region: countryItem.region,
+      // subregion: countryItem.subregion,
+      region: CountryMapper.translator(countryItem.region),
+      subregion: CountryMapper.translator(countryItem.subregion),
       area: countryItem.area.kilometers,
-      language: countryItem.languages[0].name,
+      language: countryItem.languages[0]?.name ?? 'null',
       borders: countryItem.borders
     }
     return country;
@@ -23,11 +25,14 @@ export class CountryMapper{
     return countryItems.map(this.mapCountryResponseItemToCountryItem);
   }
 
-  private static translator(name: string){
+  static translator(name: string){
+    if(!name) return '';
     if(name == 'Americas') return 'América'
     if(name == 'Europe') return 'Europa'
     if(name == 'North America') return 'Norteamérica'
     if(name == 'South America') return 'Sudamérica'
+    if(name == 'Africa') return 'África'
+    if(name == 'Oceania') return 'Oceanía'
     return name;
   }
 }
