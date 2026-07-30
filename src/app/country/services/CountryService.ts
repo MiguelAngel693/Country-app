9434d80e71ad361c9122001acae9ee39bab01c26
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-30 17:33:22 -0300

    Creado metodo para buscar por codigo de paises

--- src/app/country/services/CountryService.ts
+++ src/app/country/services/CountryService.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import { RestCountryResponse } from '../interfaces/rest-countries';
-import { catchError, map} from 'rxjs';
+import { catchError, delay, map, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 @Injectable({
   providedIn: 'root',
@@ -15,15 +15,34 @@ export class CountryService {
       headers: {
         Authorization: `Bearer ${environment.api_key}`
       }, params: {
-        q: query
+        q: query,
+        limit: 100,
       }
     }).pipe(
       map(resp => CountryMapper.mapCountryResponseArrayToCountryArray(resp.data.objects ?? [])),
+      delay(1000),
       catchError((err) => {
         throw ('Fetching error ' + err);
       })
     );
   }
+
+  public searchCountryByCode(code: string) {
+    return this.http.get<RestCountryResponse>(`${environment.countriesUrl}/codes.alpha_3/${code}`, {
+    // return this.http.get<RestCountryResponse>(`/codes.alpha_3/${code}`, {
+      headers: {
+        Authorization: `Bearer ${environment.api_key}`,
+      },
+    }).pipe(
+      map(resp => CountryMapper.mapCountryResponseArrayToCountryArray(resp.data.objects ?? [])),
+      map(countries => countries.at(0)),
+      delay(1000),
+      catchError((err) => {
+        return throwError(() => new Error('No se pudo obtener un pais con el codigo ' + code));
+      })
+    );
+  }
+
 
   searchByCapital(query: string) {
     return this.search(query, '/capitals');
