16e6e07e8be472650f500d4571936fc5d920c4f3
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-31 23:09:12 -0300

    Implementado cache

--- src/app/country/services/CountryService.ts
+++ src/app/country/services/CountryService.ts
@@ -2,13 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import { RestCountryResponse } from '../interfaces/rest-countries';
-import { catchError, delay, map, throwError } from 'rxjs';
+import { catchError, delay, map, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
+import { Country } from '../interfaces/country.interface';
+
+
 @Injectable({
   providedIn: 'root',
 })
+
 export class CountryService {
   private http = inject(HttpClient);
+  private capitalCache = new Map<string, Country[]>()
+  private countryCache = new Map<string, Country[]>()
+  private regionCache = new Map<string, Country[]>()
+  private codeCache = new Map<string, Country | undefined>()
 
   private search(query: string, endpoint: string) {
     return this.http.get<RestCountryResponse>(`${environment.countriesUrl}${endpoint}`, {
@@ -20,6 +28,19 @@ export class CountryService {
       }
     }).pipe(
       map(resp => CountryMapper.mapCountryResponseArrayToCountryArray(resp.data.objects ?? [])),
+      tap((countries) => {
+        switch (endpoint) {
+          case '':
+            this.countryCache.set(query, countries);
+            break;
+          case '/capitals':
+            this.capitalCache.set(query, countries);
+            break;
+          case '/region':
+            this.regionCache.set(query, countries);
+            break;
+        }
+      }),
       delay(1000),
       catchError((err) => {
         throw ('Fetching error ' + err);
@@ -28,8 +49,9 @@ export class CountryService {
   }
 
   public searchCountryByCode(code: string) {
+    // if(this.codeCache.has(code)) return of(this.codeCache.get(code))
     return this.http.get<RestCountryResponse>(`${environment.countriesUrl}/codes.alpha_3/${code}`, {
-    // return this.http.get<RestCountryResponse>(`/codes.alpha_3/${code}`, {
+      // return this.http.get<RestCountryResponse>(`/codes.alpha_3/${code}`, {
       headers: {
         Authorization: `Bearer ${environment.api_key}`,
       },
@@ -37,6 +59,9 @@ export class CountryService {
       map(resp => CountryMapper.mapCountryResponseArrayToCountryArray(resp.data.objects ?? [])),
       map(countries => countries.at(0)),
       delay(1000),
+      tap((country) => {
+        return this.codeCache.set(code, country);
+      }),
       catchError((err) => {
         return throwError(() => new Error('No se pudo obtener un pais con el codigo ' + code));
       })
@@ -45,12 +70,16 @@ export class CountryService {
 
 
   searchByCapital(query: string) {
+    if (this.capitalCache.has(query)) return of(this.capitalCache.get(query));
+    console.log('Buscando en el servidor')
     return this.search(query, '/capitals');
   }
   searchByCountry(query: string) {
+    if (this.countryCache.has(query)) return of(this.countryCache.get(query));
     return this.search(query, '');
   }
   searchByRegion(query: string) {
+    if (this.regionCache.has(query)) return of(this.regionCache.get(query));
     return this.search(query, '/region');
   }
 }
