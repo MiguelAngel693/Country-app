c678717f2ad1420e3b07311da07122b5a941cad8
MiguelAngel693 <miguelag_2002@hotmail.com>
2026-07-31 23:07:14 -0300

    Cambio de interfaz

--- src/app/country/pages/by-region/by-region.ts
+++ src/app/country/pages/by-region/by-region.ts
@@ -1,18 +1,20 @@
 import { Component, inject, resource, signal } from '@angular/core';
-import { SearchInput } from "../../components/search-input/search-input";
 import { List } from "../../components/list/list";
 import { CountryService } from '../../services/CountryService';
 import { firstValueFrom, of } from 'rxjs';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-by-region',
-  imports: [SearchInput, List],
+  imports: [List, RouterLink],
   templateUrl: './by-region.html',
 })
 export class ByRegion {
   country = inject(CountryService);
   query = signal('');
+  regions : string[]=["Africa","America","Asia","Europe","Oceania" ]
+  selectedRegion = signal<string>('');
 
   countryResource = rxResource({
     params: () => ({ query: this.query() }),
@@ -22,6 +24,12 @@ export class ByRegion {
       return this.country.searchByRegion(this.query())
     }
   })
+
+  selectRegion(value: string){
+    this.query.set(value);
+    this.selectedRegion.set(value);
+  }
+
   // country = inject(CountryService);
   // countries = signal<Country[]>([]);
 
